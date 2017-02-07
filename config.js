4da9158ed63217015f78a2b8e46192f7c9fb2cf2
Jarzon <j@masterj.net>
2017-02-06 20:57:00 -0500

    Adding more configs

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     name: '',
     password: '', // I don't recommend putting your password here, even more if you didn't enable 2 factors authentication
-    groupid: ''
+    groupid: '',
+    spamTimeLimit: 5, // time between messages to be considered as spamming
+    spamHistoryLimit: 3600, // Time before the comments are removed from the database in minutes
+    spamCountLimit: 4, // Minimal number of message
+    spamMessageDiff: 10 // Maximum number of letters that can differ between two comments to consider them as the same spam message
 };
