511f0f6023a8b71021f70bd80459758833950e7f
Jarzon <j@masterj.net>
2017-02-06 21:00:46 -0500

    Put the spam detection in a Class to test it

--- bot.js
+++ bot.js
@@ -4,10 +4,12 @@ var SteamCommunity = require('../node-steamcommunity/index.js');
 var ReadLine = require('readline');
 var fs = require('fs');
 var levenshtein = require('fast-levenshtein');
+var Spam = require('src/spam.js');
 
 var config = require('./config.js');
 
 var community = new SteamCommunity();
+
 var rl = ReadLine.createInterface({
     "input": process.stdin,
     "output": process.stdout
@@ -76,99 +78,7 @@ function doLogin(accountName, password, authCode, twoFactorCode, captcha) {
                 process.exit(1);
             }
 
-            var spamDiff = 10;
-            var spamCountLimit = 4;
-            var spamTimeLimit = 36000;
-
-            var commentsDB = {};
-            var spamDB = [];
-            var lastId = 0;
-
-            setInterval(function() {
-                var timeLimit = (new Date()).getTime() - 3600; // 5 mins
-
-                group.getAllComments(0, 20, function(err, comments) {// TODO: Detect that the last comment is to old and select the next page
-
-                    for (var i = comments.length; i-- > 0;) {
-                        var newComment = comments[i];
-
-                        if(newComment.commentId > lastId) {
-
-                            console.log(newComment.commentId + ": " + newComment.text);
-
-                            if(newComment.text.match("<a")) { //TODO: Add a config to toggle any spam (with or without links)
-
-                                for (let index in commentsDB) {
-                                    var commentRow = commentsDB[index];
-                                    console.log("  comment: " + commentRow.commentId + " diff: " + levenshtein.get(newComment.text, commentRow.text));
-
-                                    if(levenshtein.get(newComment.text, commentRow.text) < (newComment.text.length / spamDiff)) {
-                                        console.log("   is a Spam");
-
-                                        // Search if the ref comment is already marked as spam
-                                        var n = 0;
-                                        var result = -1;
-                                        for (let spamGroup of spamDB) {
-                                            if(spamGroup.indexOf(commentRow.commentId) > -1) {
-                                                result = n;
-                                                break;
-                                            }
-
-                                            n++;
-                                        }
-
-                                        // TODO: Use the comment id as an index
-
-                                        if(result > -1) {
-                                            console.log("   add it to related spam");
-                                            spamDB[result].push(newComment.commentId);
-                                        } else {
-                                            console.log("   Created a new spam cell");
-                                            spamDB.push([
-                                                commentRow.commentId,
-                                                newComment.commentId
-                                            ]);
-                                        }
-                                        break;
-                                    }
-                                }
-
-                                commentsDB[newComment.commentId] = newComment;
-                            }
-
-                            lastId = newComment.commentId;
-                        }
-                    }
-                });
-
-                // Clean the array from too old comments
-                for (var i = commentsDB.length; i-- > 0;) {
-                    if(commentsDB.date < timeLimit) {
-                        commentsDB.splice(i);
-                    }
-                }
-
-                // Look at the spam DB and take actions
-                spamDB.forEach((group) => {
-                    if(group.length > spamCountLimit) {
-                        var lastTime = 0;
-                        var count = 0;
-                        group.forEach((id) => {
-                            var comment = commentsDB[id];
-                            var time = commentsDB[id].date.getTime();
-                            if((time - lastTime) <= spamTimeLimit) {
-                                count++;
-
-                                if(count >= spamCountLimit) {
-                                    console.log("Ban Ban BAN!");
-                                }
-                            }
-
-                            lastTime = time;
-                        });
-                    }
-                });
-            }, 5000);
+            var spam = new Spam(config, group);
         });
 
     });
---
+++ src/spam.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var fs = require('fs');
+var SteamCommunity = require('../node-steamcommunity/index.js');
+var levenshtein = require('fast-levenshtein');
+
+var config = require('./config.js');
+
+module.exports = class Spam {
+
+    constructor(config, group) {
+        this.group = group;
+        this.config = config;
+
+        this.commentsDB = {};
+        this.spamDB = [];
+        this.lastId = 0;
+    }
+
+    loop() {
+        setInterval(() => {
+            var timeLimit = (new Date()).getTime() - this.config.spamHistoryLimit;
+
+            this.group.getAllComments(0, 20, (err, comments) => {
+
+                for (var i = comments.length; i-- > 0;) {
+                    var newComment = comments[i];
+
+                    // Look if its newer
+                    if(newComment.commentId > this.lastId) {
+
+                        if(newComment.text.match("<a")) {
+
+                            for (let key in this.commentsDB) {
+                                var commentRow = this.commentsDB[key];
+
+                                if(levenshtein.get(newComment.text, commentRow.text) < (newComment.text.length / this.config.spamMessageDiff)) {
+
+                                    // Search if the ref comment is already marked as spam
+                                    var n = 0;
+                                    var spamKey = -1;
+                                    for (let spamGroup of this.spamDB) {
+                                        if(spamGroup.indexOf(commentRow.commentId) > -1) {
+                                            spamKey = n;
+                                            break;
+                                        }
+
+                                        n++;
+                                    }
+
+                                    // TODO: Use the comment id as an index
+
+                                    if(result > -1) {
+                                        console.log("   add it to related spam");
+                                        this.spamDB[spamKey].push(newComment.commentId);
+                                    } else {
+                                        console.log("   Created a new spam cell");
+                                        this.spamDB.push([
+                                            commentRow.commentId,
+                                            newComment.commentId
+                                        ]);
+                                    }
+                                    break;
+                                }
+                            }
+
+                            this.commentsDB[newComment.commentId] = newComment;
+                        }
+
+                        this.lastId = newComment.commentId;
+                    }
+                }
+            });
+
+            // Clean the array from too old comments
+            for (let comment in this.commentsDB) {
+                if(commentsDB.date < timeLimit) {
+                    delete this.commentsDB[comment];
+                } else break;
+            }
+
+            // Look at the spam DB and take actions
+            this.spamDB.forEach((group) => {
+                if(group.length > this.config.spamCountLimit) {
+                    var lastTime = 0;
+                    var count = 0;
+                    group.forEach((id) => {
+                        var comment = this.commentsDB[id];
+                        var time = this.commentsDB[id].date.getTime();
+                        if((time - lastTime) <= this.config.spamTimeLimit) {
+                            count++;
+
+                            if(count >= this.config.spamCountLimit) {
+                                console.log("Ban Ban BAN!");
+                            }
+                        }
+
+                        lastTime = time;
+                    });
+                }
+            });
+        }, 5000);
+    }
+};
