8b14f25fdd3ec2669e7c2b2900c37dc070232f11
Jarzon <j@masterj.net>
2017-02-09 19:57:24 -0500

    Added tests

--- test/botTests.js
+++ test/botTests.js
@@ -31,19 +31,40 @@ function delay(callback) {
     setTimeout(callback, 1);
 }
 
-var group = {
-    getAllComments: (from, count, callback) => {
+class GroupMock
+{
+    constructor()
+    {
+        this.deleted = [];
+    }
+
+    getAllComments(from, count, callback)
+    {
         callback(null, commentsOutput);
     }
-};
+
+    deleteComment(cid, callback)
+    {
+        this.deleted.push(cid);
+    }
+
+    getDeleted()
+    {
+        var output = this.deleted;
+        this.deleted = [];
+        return output;
+    }
+}
+
+var group = new GroupMock();
 
 var config = {
     name: '',
     password: '',
     groupid: '',
-    spamTimeLimit: 5,
-    spamHistoryLimit: 3600,
-    spamCountLimit: 4,
+    spamTimeLimit: 5 * 1000,
+    spamHistoryLimit: 360 * 1000,
+    spamCountLimit: 2,
     spamMessageDiff: 10,
     spamLookRate: 1
 };
@@ -56,12 +77,12 @@ beforeEach(() => {
 });
 
 describe('Spam', () => {
-    describe('#detectSpam()', () => {
+    describe('#getGroupComments()', () => {
 
         it('should add a comment with a link in commentsDB', (done) => {
             addComment("Master J", 0, getDate(), 1, 'Spam message <a href="localhost">link</a>');
 
-            spam.detectSpam();
+            spam.getGroupComments();
 
             assert.equal(Object.keys(spam.commentsDB).length, 1);
             done();
@@ -72,7 +93,7 @@ describe('Spam', () => {
             addComment("Master J", "MasterJibus", getDate(), 1, 'Spam message <a href="localhost">link</a>');
             addComment("Master J", "MasterJibus", getDate(), 2, 'Spam message <a href="localhost">link</a>');
 
-            spam.detectSpam();
+            spam.getGroupComments();
 
             assert.equal(spam.spamDB.length, 1);
             assert.equal(spam.spamDB[0].length, 2);
@@ -84,7 +105,7 @@ describe('Spam', () => {
             addComment("Master J", "MasterJibus", getDate(), 1, 'Spam message <a href="localhost">link</a>');
             addComment("Master J", "MasterJibus", getDate(), 2, 'Test comment with a <a href="localhost">link</a>');
 
-            spam.detectSpam();
+            spam.getGroupComments();
 
             assert.equal(spam.spamDB.length, 0);
             done();
@@ -97,10 +118,56 @@ describe('Spam', () => {
 
             addComment("Master J", "MasterJibus", getDate(), 3, 'Spam message <a href="localhost">link</a> 3');
 
-            spam.detectSpam();
+            spam.getGroupComments();
 
             assert.equal(spam.spamDB.length, 1);
             assert.equal(spam.spamDB[0].length, 3);
+            done();
+
+        });
+    });
+
+    describe('#clearComments()', () => {
+        it('should delete comments that are too old', (done) => {
+            var old = config.spamHistoryLimit + 1000;
+
+            addComment("Master J", "MasterJibus", getDate(old), 1, 'Lorem ipsum dolor sit amet, <a href="localhost">link</a>');
+            addComment("Master J", "MasterJibus", getDate(old), 2, 'pri falli corrumpit ullamcorper id <a href="localhost">link</a>');
+
+            addComment("Master J", "MasterJibus", getDate(), 3, 'graece invidunt ex mel. <a href="localhost">link</a>');
+
+            spam.getGroupComments();
+
+            assert.equal(Object.keys(spam.commentsDB).length, 3);
+
+            spam.clearComments();
+
+            assert.equal(Object.keys(spam.commentsDB).length, 1);
+            done();
+
+        });
+    });
+
+    describe('#spamAction()', () => {
+        it('should delete spam comments that are within the config time span range', (done) => {
+            addComment("Master J", "MasterJibus", getDate(), 1, 'Spam message <a href="localhost">link</a>');
+            addComment("Master J", "MasterJibus", getDate(), 2, 'Spam message <a href="localhost">link</a> 2');
+
+            addComment("Master J", "MasterJibus", getDate(), 3, 'Spam message <a href="localhost">link</a> 3');
+
+            spam.getGroupComments();
+
+            assert.equal(spam.spamDB.length, 1);
+            assert.equal(spam.spamDB[0].length, 3);
+
+            spam.spamAction();
+
+            var deleted = group.getDeleted();
+
+            assert.equal(deleted[0], 1);
+            assert.equal(deleted[1], 2);
+            assert.equal(deleted[2], 3);
+
             done();
 
         });
