b4e2f14ea3a392a1bad6e1b611aa0c4599716c91
Jarzon <j@jasonvaillancourt.ca>
2018-06-04 01:56:51 -0400

    Fix spamLookRate being in ms not in seconds

--- src/spam.js
+++ src/spam.js
@@ -25,7 +25,7 @@ module.exports = class Spam
             this.getGroupComments();
             this.clearComments();
             this.spamAction();
-        }, this.config.spamLookRate);
+        }, this.config.spamLookRate * 1000);
     }
 
     getGroupComments()
